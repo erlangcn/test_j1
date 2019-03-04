429e4ce5434e1ad83a564c9af211e7c06940df35
qichang <1993887040@qq.com>
2019-03-05 01:10:52 +0800

    erlangcn commit 2

--- src/com/test/MyMain.java
+++ src/com/test/MyMain.java
@@ -5,5 +5,7 @@ public class MyMain {
         System.out.println("init");
         System.out.println("commit on b1");
         System.out.println("commit by erlangcn on b1");
+        System.out.println("temp change on b1");
+
     }
 }
