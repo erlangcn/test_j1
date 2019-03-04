99edfd6f3bb8f6d677247b41c0b59d70d14f4ec0
qichang <1993887040@qq.com>
2019-03-04 15:53:18 +0800

    by erlangcn on b1

--- src/com/test/MyMain.java
+++ src/com/test/MyMain.java
@@ -4,5 +4,6 @@ public class MyMain {
     public static void main(String[] args){
         System.out.println("init");
         System.out.println("commit on b1");
+        System.out.println("commit by erlangcn on b1");
     }
 }
